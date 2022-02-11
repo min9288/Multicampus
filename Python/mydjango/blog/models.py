da5cb68be55882ded6e72a2c893f15f49e07e5d4
minuda <min9288@gmail.com>
2022-02-11 17:55:41 +0900

    code update

--- Python/mydjango/blog/models.py
+++ Python/mydjango/blog/models.py
@@ -15,3 +15,16 @@ class Post( models.Model):
     # 게시일
     published_date = models.DateTimeField (blank=True , null=True)
 
+    # 삭제할 예정 마이그레이션 테스트 하기 위함
+    # test = models.TextField()
+
+    # Model 클래스에 정의된 __str__ 함수를 재정의(title 필드값을 리턴함)
+    def __str__(self):
+        return self.title + '(' + str(self.id) + ')'
+
+    # published_date 필드에 현재날짜를 저장하는 메서드
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+
--- Python/mydjango/blog/views.py
+++ Python/mydjango/blog/views.py
@@ -1,3 +1,14 @@
+from django.http import  HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+# View 내에 선언된 함수로 인자로 HttpRequest 하는 객체를 Django가 전달해준다.
+def post_list(request):
+    my_name = '장고웹프레임워크'
+    http_method = request.method
+    return HttpResponse('''
+        <h2>Welcome {name}</h2>
+        <p>Http Method : {method}</p>
+        <p>Http headers User-Agent : {header}</p>
+        <p>Http Path : {mypath}</p>
+    '''.format(name=my_name, method=http_method, header=request.headers['user-agent'], mypath=request.path))
